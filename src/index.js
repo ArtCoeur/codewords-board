b069dce6ba7c7329c329f1f32bd410b890890811
Tim Rodger <tim.rodger@gmail.com>
2015-04-01 17:27:34 +0100

    Adds a handler for incoming messages

--- src/index.js
+++ src/index.js
@@ -1,24 +1,46 @@
 var logger = require('./lib/logger'),
-    rabbitmq = require('rabbit.js');
+    rabbitmq = require('rabbit.js'),
+    handler = require('./lib/handler');
 
 logger.info('board handler running');
 
 // wait until rabbitmq can accept connections, somehow
 
-var context = rabbitmq.createContext('amqp://'+process.env.RABBITMQ_PORT_5672_TCP_ADDR+':5672');
+function doConnect(){
 
-context.on('ready', function() {
-    logger.info('connected');
-    // subscribe to facts
-    var sub = context.socket('SUB'), pub = context.socket('PUB');
+    try {
+        var context = rabbitmq.createContext(
+            'amqp://' + process.env.RABBITMQ_PORT_5672_TCP_ADDR + ':' + process.env.RABBITMQ_PORT_5672_TCP_PORT
+        );
 
-    // test connection
-    pub.write(JSON.stringify({name: 'board-ready'}), 'utf8');
+        context.on('ready', function() {
 
-    // deal with facts as they come in
-    sub.on('data', function(body) {
-        logger.info("fact : " +  JSON.parse(body));
+            logger.info('connected');
 
-    });
-});
+            // subscribe to pub and sub queues
+            var sub = context.socket('SUB'),
+                pub = context.socket('PUB');
 
+            sub.connect('events', function(){
+                logger.info('Connected to events');
+
+                // deal with facts as they come in
+                sub.on('data', function (body) {
+                    logger.info("new fact : " + body);
+                    handler.handleFact(pub, JSON.parse(body));
+                });
+            });
+        });
+
+        return true;
+
+    } catch (err){
+        logger.error(err);
+
+        setTimeout(doConnect, 3000);
+        return false;
+    }
+}
+
+// hack to wait till rabbitmq is up
+setTimeout(doConnect, 10000);
---
+++ src/lib/handler.js
@@ -0,0 +1,9 @@
+
+// deal with facts as they arrive
+
+exports.handleFact(pub, fact){
+    if (fact.name == 'new-board'){
+        // extract words from board data & cells
+        // pub.write(json, 'utf8');
+    }
+}
