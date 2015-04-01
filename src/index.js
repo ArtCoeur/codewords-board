573ab28f8a91d3722407a54b035c27d5b7b64f12
Tim Rodger <tim.rodger@gmail.com>
2015-04-01 16:04:48 +0100

    Adds logger code and subscribes from index

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 var logger = require('./lib/logger'),
     rabbitmq = require('rabbit.js');
 
+logger.info('board handler running');
+
 // wait until rabbitmq can accept connections, somehow
 
 var context = rabbitmq.createContext('amqp://'+process.env.RABBITMQ_PORT_5672_TCP_ADDR+':5672');
@@ -8,6 +10,15 @@ var context = rabbitmq.createContext('amqp://'+process.env.RABBITMQ_PORT_5672_TC
 context.on('ready', function() {
     logger.info('connected');
     // subscribe to facts
-    // deal with them as they come in
+    var sub = context.socket('SUB'), pub = context.socket('PUB');
+
+    // test connection
+    pub.write(JSON.stringify({name: 'board-ready'}), 'utf8');
+
+    // deal with facts as they come in
+    sub.on('data', function(body) {
+        logger.info("fact : " +  JSON.parse(body));
+
+    });
 });
 
